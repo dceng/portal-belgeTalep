c8d694ede3f011fcc39d7ad47a0de50a125cd677
Duygu Çınar <duygucinar-1641@hotmail.com>
2018-12-07 12:19:41 +0300

    initial commit

---
+++ controllers/console/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+namespace kouosl\belgetalep\controllers\console;
+
+
+/**
+ * Default controller for the `belgetalep` module
+ */
+class DefaultController extends \kouosl\base\controllers\console\BaseController
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('_index');
+    }
+}
