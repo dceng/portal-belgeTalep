d7029d12eff080852299f3be72b28db451daa897
Duygu Çınar <duygucinar-1641@hotmail.com>
2019-01-05 18:26:31 +0300

    Index files changed.

--- views/frontend/default/index.php
+++ views/frontend/default/index.php
@@ -2,11 +2,11 @@
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Kou Osl Yii2 App</h1>
+        <h1>BELGE TALEP</h1>
 
-        <p class="lead">Örnek uygulamayı başarılı bir şekilde çalıştırdınız.</p>
+        <p class="lead">Öğrenci Belge Talep Modülü</p>
 
-        <p><a class="btn btn-lg btn-success" href="#">Modüller ve konfürgasyon!</a></p>
+        <p><a class="btn btn-lg btn-success" href="admin/belgetalep/document">Belge Talep Et</a></p>
     </div>
 
 </div>
